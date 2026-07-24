44924c2dc73e619e2177de81ebb44371db0c3b8b
Maiko Kudo <124079294+azs4n10@users.noreply.github.com>
2026-07-24 14:26:04 +0900

    Deploy web build for 2fda5f8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1911354532" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2471487723" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
